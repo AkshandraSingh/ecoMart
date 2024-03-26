f6dc50b4870f130e27ed6ce9bb93b3880277fe47
Ishan Singh <nameste380@gmail.com>
2024-03-26 22:52:54 +0530

    addUpdateCategoryAPI

--- admin_app/controllers/categoryController.js
+++ admin_app/controllers/categoryController.js
@@ -1,6 +1,7 @@
 const categoryModel = require('../../models/categoryModel')
 
 module.exports = {
+    //? Add Category API For Admin 👀
     addCategory: async (req, res) => {
         try {
             const categoryData = new categoryModel(req.body)
@@ -25,5 +26,35 @@ module.exports = {
                 error: error.message,
             })
         }
-    }
+    },
+    //? Update Category API For Admin 🎯
+    updateCategory: async (req, res) => {
+        try {
+            const { categoryId } = req.params
+            const isCategoryExist = await categoryModel.findOne({
+                categoryId: req.params.id
+            })
+            if (!isCategoryExist) {
+                return res.status(400).send({
+                    success: false,
+                    message: "Category does not exist!",
+                })
+            }
+            const categoryData = await categoryModel.findOneAndUpdate({
+                categoryId: req.params.id
+            }, req.body, {
+                new: true
+            })
+            res.status(200).send({
+                success: true,
+                message: "Category updated successfully!",
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Server error!",
+                error: error.message,
+            })
+        }
+    },
 }
--- admin_app/routes/categoryRoute.js
+++ admin_app/routes/categoryRoute.js
@@ -5,5 +5,6 @@ const categoryController = require('../controllers/categoryController')
 const categoryRouter = express.Router()
 
 categoryRouter.post('/addCategory', categoryController.addCategory)
+categoryRouter.patch('/updateCategory/:categoryId', categoryController.updateCategory)
 
 module.exports = categoryRouter;
