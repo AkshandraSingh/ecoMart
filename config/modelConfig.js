92976f18ed6d2421dbbb811fcf9610a26b2da5cb
Ishan Singh <nameste380@gmail.com>
2024-03-31 14:05:47 +0530

    addedMainLogger

--- config/modelConfig.js
+++ config/modelConfig.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
+const mainLogger = require('../utils/mainLogger')
+
 mongoose.connect(process.env.URL)
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected successfully')
+    mainLogger.info('Mongoose is connected successfully')
 })
 
 mongoose.connection.on('error', (error) => {
     console.log('Mongoose Error!')
     console.log(`Error: ${error}`)
+    mainLogger.error('Mongoose Error!')
+    mainLogger.error(`Error: ${error}`)
 })
